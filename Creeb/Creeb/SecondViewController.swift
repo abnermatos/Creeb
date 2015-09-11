24dc32819aaa37318062550b4dcb2bde30064f18
Abner <abnerjvmatos@gmail.com>
2015-09-11 14:37:04 -0400

    Creeb is ON

---
+++ Creeb/Creeb/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Creeb
+//
+//  Created by Abner Josue Viana Matos on 11/09/15.
+//  Copyright © 2015 BEPiD FUCAPI. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Creeb/CreebTests/CreebTests.swift
@@ -0,0 +1,36 @@
+//
+//  CreebTests.swift
+//  CreebTests
+//
+//  Created by Abner Josue Viana Matos on 11/09/15.
+//  Copyright © 2015 BEPiD FUCAPI. All rights reserved.
+//
+
+import XCTest
+@testable import Creeb
+
+class CreebTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
